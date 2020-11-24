176b5f226e4cddbeedade33a84f18145c2db46ff
rawrrhub <74896156+rawrrhub@users.noreply.github.com>
2020-11-23 22:48:31 -0800

    create hangman
    this is the very beginnings of making a hangman game..  so far it just replaces characters in the word you choose with dashes

---
+++ startofhangman.py
@@ -0,0 +1,8 @@
+def display_blanks( word):
+    blanks = "-" * len(word)
+    print(blanks)
+
+print("Puzzle 1:")
+display_blanks("treehouse")
+print("Puzzle 2:")
+display_blanks( "python") #this can be start of hangman.. will come back to this and try to actually make the game
