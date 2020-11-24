3814030041676e793559441f615f2136214b9f6d
rawrrhub <74896156+rawrrhub@users.noreply.github.com>
2020-11-23 22:50:11 -0800

    create passwordinput.py
    This is a very rough beginnings of a password input with attempt counter that stops after 3 attempts

---
+++ passwordinput.py
@@ -0,0 +1,11 @@
+import sys #we need sys and sys.exit to keep track of attempt counts and cause a break
+
+MASTER_PASSWORD = 'opensesame' 
+password = input("Please enter the super secret password: ")
+attempt_count = 1
+while password != MASTER_PASSWORD:
+    if attempt_count > 3:
+        sys.exit("Too many invalid password attempts")
+    password = input("Invalid password, try again: ")
+    attempt_count += 1 
+print("Welcome to secret town")
